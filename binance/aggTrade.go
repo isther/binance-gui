24c00b27102e5752a9420c8995442760c689e4be
isther <isther@qq.com>
2022-07-16 15:28:51 +0800

    modify color

--- binance/aggTrade.go
+++ binance/aggTrade.go
@@ -2,13 +2,13 @@ package binance
 
 import (
 	"fmt"
-	"image/color"
 	"strconv"
 	"time"
 
 	"github.com/AllenDang/giu"
 	libBinance "github.com/adshao/go-binance/v2"
 	"github.com/isther/binanceGui/console"
+	"github.com/isther/binanceGui/global"
 )
 
 var (
@@ -30,6 +30,8 @@ func runOneAggTradeDepth() (chan struct{}, chan struct{}) {
 	)
 
 	wsAggTradeHandler := func(event *libBinance.WsAggTradeEvent) {
+		AggTradePrice = priceFloat648Point(event.Price)
+		fmt.Println(AggTradePrice)
 		globalWsAggTradeServerC <- event
 	}
 
@@ -51,13 +53,12 @@ func buildAggTradeTable() []*giu.TableRowWidget {
 		rows        []*giu.TableRowWidget
 		aggTradeNew = <-globalWsAggTradeServerC
 	)
-	AggTradePrice = priceFloat648Point(aggTradeNew.Price)
 
 	rows = append(rows, giu.TableRow(
 		giu.Label("时间"),
 		giu.Label("价格"),
 		giu.Label("成交额"),
-	).BgColor(&(color.RGBA{0x33, 0x33, 0xff, 0xff})))
+	).BgColor(global.PURPLE))
 
 	price, err := strconv.ParseFloat(aggTradeNew.Price, 64)
 	if err != nil {
@@ -68,9 +69,9 @@ func buildAggTradeTable() []*giu.TableRowWidget {
 		console.ConsoleInstance.Write(fmt.Sprintf("Error: %v", err))
 	}
 
-	eColor := color.RGBA{0xff, 0x33, 0x00, 0xff}
+	eColor := global.GREEN
 	if aggTradeNew.IsBuyerMaker {
-		eColor = color.RGBA{0x66, 0xcc, 0x00, 0xff}
+		eColor = global.RED
 	}
 
 	timeStr := time.Unix(aggTradeNew.Time/1e3, 0)
