0e2c64bc23e0e196591cf79c46e2aa5a6e2c9642
isther <isther@qq.com>
2022-07-16 15:26:09 +0800

    add volatilityRatios input

--- window.go
+++ window.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"image/color"
 	"math/rand"
 	"time"
 
@@ -13,6 +12,8 @@ import (
 )
 
 var (
+	volatilityRatiosInputSize float32 = 100
+
 	symbol  = binance.AccountInstance.Symbol
 	symbol1 = binance.AccountInstance.One.Asset
 	symbol2 = binance.AccountInstance.Two.Asset
@@ -39,14 +40,15 @@ func mainWindow() {
 		Layout(
 			giu.PrepareMsgbox(),
 			giu.MenuBar().Layout(
-				giu.Menu("设置").Layout(
-					giu.MenuItem("Api").OnClick(func() { giu.Msgbox("Info", "构建中...") }),
-					giu.MenuItem("本地代理").OnClick(func() { giu.Msgbox("Info", "构建中...") }),
+				giu.Menu("说明").Layout(
+					giu.MenuItem("快捷键").OnClick(func() {
+						giu.Msgbox("快捷键说明", hotKeyTip).Buttons(giu.MsgboxButtonsOk)
+					}),
 				),
 			),
-			giu.SplitLayout(giu.DirectionHorizontal, 1200, //H
-				giu.SplitLayout(giu.DirectionHorizontal, 600, //H
-					giu.SplitLayout(giu.DirectionVertical, 600, //V
+			giu.SplitLayout(giu.DirectionHorizontal, 1350, //H
+				giu.SplitLayout(giu.DirectionVertical, 600, //V
+					giu.SplitLayout(giu.DirectionHorizontal, 750, //V
 						giu.TabBar().TabItems(
 							giu.TabItem("K线").Layout(
 								giu.Plot("Plot Time Axe 时间线").Size(580, 540).AxisLimits(timeDataMin, timeDataMax, 0, 1, giu.ConditionOnce).XAxeFlags(giu.PlotAxisFlagsTime).Plots(
@@ -55,19 +57,87 @@ func mainWindow() {
 								),
 							),
 							giu.TabItem("持仓明细").Layout(),
+							giu.TabItem("波动比设置").Layout(),
+							giu.TabItem("波动比设置").Layout(giu.Style().
+								SetColor(giu.StyleColorBorder, global.BLUE).
+								SetStyle(giu.StyleVarFramePadding, 10, 10).
+								To(
+									giu.Column(
+										giu.Label("卖"),
+										giu.Row(
+											giu.Label("06 - 10"),
+											giu.InputFloat(&global.VolatilityRatiosSale[5]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosSale[6]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosSale[7]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosSale[8]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosSale[9]).Size(volatilityRatiosInputSize),
+										),
+										giu.Row(
+											giu.Label("11 - 15"),
+											giu.InputFloat(&global.VolatilityRatiosSale[10]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosSale[11]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosSale[12]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosSale[13]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosSale[14]).Size(volatilityRatiosInputSize),
+										),
+										giu.Row(
+											giu.Label("16 - 20"),
+											giu.InputFloat(&global.VolatilityRatiosSale[15]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosSale[16]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosSale[17]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosSale[18]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosSale[19]).Size(volatilityRatiosInputSize),
+										),
+										giu.Label("买"),
+										giu.Row(
+											giu.Label("06 - 10"),
+											giu.InputFloat(&global.VolatilityRatiosBuy[5]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosBuy[6]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosBuy[7]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosBuy[8]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosBuy[9]).Size(volatilityRatiosInputSize),
+										),
+										giu.Row(
+											giu.Label("11 - 15"),
+											giu.InputFloat(&global.VolatilityRatiosBuy[10]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosBuy[11]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosBuy[12]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosBuy[13]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosBuy[14]).Size(volatilityRatiosInputSize),
+										),
+										giu.Row(
+											giu.Label("16 - 20"),
+											giu.InputFloat(&global.VolatilityRatiosBuy[15]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosBuy[16]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosBuy[17]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosBuy[18]).Size(volatilityRatiosInputSize),
+											giu.InputFloat(&global.VolatilityRatiosBuy[19]).Size(volatilityRatiosInputSize),
+										),
+									),
+								)),
 						),
-						giu.TabBar().TabItems(
-							giu.TabItem("输出").Layout(
-								giu.Label(console.ConsoleInstance.Read()),
+						giu.SplitLayout(giu.DirectionHorizontal, 300, //H
+							giu.TabBar().TabItems(
+								giu.TabItem("当前挂单").Layout(
+									giu.SplitLayout(giu.DirectionVertical, 280,
+										giu.Table().Freeze(0, 1).FastMode(true).Size(270, 300).Rows(binance.GetOpenSaleOrdersTable()...),
+										giu.Table().Freeze(0, 1).FastMode(true).Size(270, 300).Rows(binance.GetOpenBuyOrdersTable()...),
+									),
+								),
+							),
+							giu.TabBar().TabItems(
+								giu.TabItem("成交明细").Layout(
+									giu.Table().Freeze(0, 1).FastMode(true).Size(270, 555).Rows(binance.GetWsAggTradeTable()...),
+								),
 							),
 						),
 					),
-					giu.SplitLayout(giu.DirectionVertical, 600, //V
-						giu.SplitLayout(giu.DirectionHorizontal, 300, //V
-							giu.TabBar().TabItems(giu.TabItem("当前挂单").Layout()),
-							giu.TabBar().TabItems(
-								giu.TabItem("成交明细").Layout(),
+					giu.SplitLayout(giu.DirectionHorizontal, 800, //V
+						giu.TabBar().TabItems(
+							giu.TabItem("终端").Layout(
+								giu.Label(console.ConsoleInstance.Read()),
 							),
+							giu.TabItem("历史订单").Layout(),
 						),
 						giu.SplitLayout(giu.DirectionVertical, 125, //V
 							giu.TabBar().TabItems(
@@ -109,19 +179,30 @@ func mainWindow() {
 										giu.InputInt(&global.Average).Size(240),
 									),
 									giu.Style().
-										SetColor(giu.StyleColorBorder, color.RGBA{0x36, 0x74, 0xD5, 255}).
+										SetColor(giu.StyleColorBorder, global.BLUE).
 										SetStyle(giu.StyleVarFramePadding, 10, 10).
 										To(
 											giu.Row(
-												giu.Button(fmt.Sprintf("分仓金额: %s: %.8f / %s: %.8f",
-													binance.AccountInstance.One.Asset, global.AverageSymbol1Amount,
-													binance.AccountInstance.Two.Asset, global.AverageSymbol2Amount)),
-											),
-											giu.Row(
-												giu.Button("交易热键状态(Enter): "+global.GetHotKeyStatus()).OnClick(func() { global.HotKeyRun = !global.HotKeyRun }),
-												giu.Button("交易模式(Tab): "+global.GetTradeMode()).OnClick(func() { global.ReverseTradeMode() }),
+												giu.Button(fmt.Sprintf("单仓数量: %s: %.8f ",
+													binance.AccountInstance.One.Asset, global.AverageSymbol1Amount)),
 											),
 										),
+									giu.Row(
+										giu.Style().
+											SetColor(giu.StyleColorBorder, global.BLUE).
+											SetStyle(giu.StyleVarFramePadding, 10, 10).
+											To(
+												giu.Row(
+													giu.Button("交易模式(Tab): "+global.GetTradeMode()).OnClick(func() { global.ReverseTradeMode() }),
+												),
+											),
+										giu.Style().
+											SetColor(giu.StyleColorBorder, global.HotKeyColor).
+											SetStyle(giu.StyleVarFramePadding, 10, 10).
+											To(
+												giu.Button("交易热键状态(空格): "+global.GetHotKeyStatus()).OnClick(func() { global.ReverseHotKeyStatus() }),
+											),
+									),
 								),
 							)),
 						),
@@ -131,7 +212,11 @@ func mainWindow() {
 					giu.Column(
 						giu.TabBar().TabItems(
 							giu.TabItem("订单簿2").Layout(
-								giu.Table().FastMode(true).Size(270, 840).Rows(binance.GetWsPartialDepthTable()...),
+								giu.Column(
+									giu.Table().FastMode(true).Size(270, 420).Rows(binance.GetWsPartialDepthBuyTable()...),
+									giu.Label(fmt.Sprintf("实时价格: %s", binance.AggTradePrice)),
+									giu.Table().FastMode(true).Size(270, 420).Rows(binance.GetWsPartialDepthSaleTable()...),
+								),
 							),
 						),
 					),
